0355172e73ec0a9da1d7d7cc9c57aba54a76a447
seonghoYi <sh.Lee4781@gmail.com>
2022-01-27 20:55:27 +0900

    CLI 인터페이스 추가

--- src/hw/hw_def.h
+++ src/hw/hw_def.h
@@ -7,11 +7,18 @@
 
 
 #define _USE_HW_I2C
-#define 		 HW_I2C_CH_MAX 					1
+#define 		 HW_I2C_MAX_CH 					1
 
 
 #define _USE_HW_UART
 #define      HW_UART_MAX_CH         1
+
+
+#define _USE_HW_CLI
+#define      HW_CLI_CMD_LIST_MAX    24
+#define      HW_CLI_CMD_NAME_MAX    16
+#define      HW_CLI_LINE_HIS_MAX    4
+#define      HW_CLI_LINE_BUF_MAX    64
 
 
 #define _USE_HW_SSD1306
