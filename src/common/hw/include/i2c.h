171d35ea6149b7bdcd8a95fe3ef167c41c02393b
seonghoYi <sh.Lee4781@gmail.com>
2022-01-27 20:57:13 +0900

    i2c 스캔 기능 추가

--- src/common/hw/include/i2c.h
+++ src/common/hw/include/i2c.h
@@ -9,7 +9,7 @@ extern "C" {
 #include "hw_def.h"
 
 #ifdef _USE_HW_I2C
-#define			I2C_CH_MAX			HW_I2C_CH_MAX
+#define			I2C_MAX_CH			HW_I2C_MAX_CH
 
 
 
@@ -17,6 +17,7 @@ extern "C" {
 
 bool i2cInit();
 bool i2cBegin(uint8_t ch, uint32_t freq_khz);
+bool i2cIsDeviceReady(uint8_t ch, uint8_t dev_addr);
 uint32_t i2cWrite(uint8_t ch, uint16_t dev_addr, uint8_t *p_data, uint16_t size);
 uint32_t i2cRead(uint8_t ch, uint16_t dev_addr, uint8_t *p_data, uint16_t size);
 uint32_t i2cMemWrite(uint8_t ch, uint16_t dev_addr, uint16_t mem_addr, uint16_t mem_addr_size, uint8_t *p_data, uint16_t size);
